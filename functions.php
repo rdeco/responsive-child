4ca49d7e7ffd27ac96c51a45f1094a2be4f09f6c
rdeco <rdecodesigns@gmail.com>
2015-09-09 13:37:58 -1000

    added function to change character length for blog archive excerpt. added function to rename tab titles for product page

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@
 function rdeco_jewelry_styles() {	
 	wp_enqueue_style ('googlefonts-css', 'http://fonts.googleapis.com/css?family=Adamina|Lato' );
 	wp_enqueue_style ('font-awesome', 'https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css' );
-		
+	wp_enqueue_style ('add-this', 'https://s7.addthis.com/js/300/addthis_widget.js#pubid=ra-508cf07b372297e2' );		
 }
 add_action('wp_enqueue_scripts', 'rdeco_jewelry_styles');
 
@@ -24,4 +24,26 @@ add_action('wp_enqueue_scripts', 'rdeco_jewelry_scripts');
 
 
 
+/*---------------------------------------------------*
+ * Excerpt character length for blog archive page *
+ *---------------------------------------------------*/
+ 
+function custom_excerpt_length( $length ) {
+        return 60;
+    }
+    add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
+    
+    
+/*-----------------------------------------------------------*
+ * Renamed product info tab to "reviews" on product page *
+ *-----------------------------------------------------------*/
+     
+add_filter( 'woocommerce_product_tabs', 'woo_rename_tabs', 98 );
+	function woo_rename_tabs( $tabs ) {
+
+		$tabs['reviews']['title'] = __( 'Comments' );	// Rename the description tab
+
+	return $tabs;
+}
+    
 ?>
