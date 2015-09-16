dcbd4df716a46fff2c94aebc331631ea8272485e
rdeco <rdecodesigns@gmail.com>
2015-09-16 13:51:12 -1000

    initial commit

---
+++ about-us-page.php
@@ -0,0 +1,60 @@
+<?php
+    /*
+        Template Name: About Us Page
+    */
+    
+    get_header(); ?>
+            <div id="about-me" class="about-me">
+         	<?php
+		 		$args = array(
+		 			'post_type' => 'about-us'	
+            );
+            $query = new WP_Query( $args );
+                while( $query->have_posts() ) :
+                    $query->the_post();
+                    if ($query->current_post % 2 == 0):
+            ?>
+            
+			<div class="about-container">
+                <section class="about-section">
+					<article class="about-article right">	                       
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_field('summary'); ?>	                     
+                    </article>	
+					<div id="about-content" class="about-content left"> 
+						<p class="about-read-more left">read more</p>
+	                    <figure class="about-figure img-left">		
+	                        <?php  the_post_thumbnail('large'); ?>		
+	                    </figure>
+						<?php the_content(); ?>
+					</div>
+                </section>
+        	</div>
+        	
+            <?php else: ?>
+            
+            <div class="about-container">
+                <section class="about-section">
+                
+                	<article class="about-article left">	                      
+                        <h2><?php the_title(); ?></h2>
+                        <?php the_field('summary');?>	                       
+                    </article>
+
+					<div id="about-content" class="about-content right"> 
+						<p class="about-read-more right">read more</p>
+	                    <figure class="about-figure img-right">			
+	                        <?php the_post_thumbnail('large'); ?>
+						</figure>
+						<?php the_content(); ?>
+					</div>
+                </section>
+            </div>     
+ 
+            <?php endif; ?>
+            <?php endwhile; ?>
+           
+        </div> <!-- .about-me-->
+
+<?php get_footer(); ?>
+
