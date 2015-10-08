82e65e8e6d6cd452d4901e75503153ab98fc8f6f
rdeco <rdecodesigns@gmail.com>
2015-10-08 13:07:11 -1000

    added site title and tagline code

--- full-width-page.php
+++ full-width-page.php
@@ -25,43 +25,53 @@ get_header(); ?>
 
 <div id="content-full" class="grid col-940">
 
-	<?php if ( have_posts() ) : ?>
-
-		<?php while( have_posts() ) : the_post(); ?>
-
-			<?php get_template_part( 'loop-header', get_post_type() ); ?>
-
-				<h1 class="page-h1"><?php the_title(); ?></h1>
-			<?php responsive_entry_before(); ?>
-			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php responsive_entry_top(); ?>
-
-
-				<div class="post-entry">
-					<?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
-					<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
-				</div>
-				<!-- end of .post-entry -->
-
-
-				<?php responsive_entry_bottom(); ?>
-			</div><!-- end of #post-<?php the_ID(); ?> -->
-			<?php responsive_entry_after(); ?>
-
-
-
-		<?php
-		endwhile;
-
-		get_template_part( 'loop-nav', get_post_type() );
-
-	else :
-
-		get_template_part( 'loop-no-posts', get_post_type() );
-
-	endif;
+	<?php 
+		if ( have_posts() ) :  while( have_posts() ) : the_post(); 
+		get_template_part( 'loop-header', get_post_type() ); 
+		responsive_entry_before(); 
 	?>
+	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php responsive_entry_top(); ?>
 
+		<header class="landing-page">
+			<h1 class="landing-page-header"><?php bloginfo('name'); ?></h1>
+			<p class="landing-page-intro"><?php bloginfo('description'); ?></p>
+			<p class="landing-page-second-intro">restored  <i class="fa fa-circle front-page"></i> repurposed  <i class="fa fa-circle front-page"></i> reinvented</p>
+		</header>
+		<section id="dg-container" class="dg-container">
+			<div class="dg-wrapper">
+				<a id="img1" href="#">
+					<div>Blue Angel</div>
+				</a>				
+				<a id="img2" href="#">
+					<div>On the Town</div>
+				</a>			
+				<a id="img3"href="#">
+					<div>Midnight Rendevous</div>
+				</a>
+			</div>
+			<nav>	
+				<span class="dg-prev"><</span>
+				<span class="dg-next">></span>
+			</nav>
+		</section>	
+		<div class="landing-page-btn-wrapper">	
+			<a href="<?php bloginfo('url'); ?>/shop"><button class="button button--shikoba"><i class="button__icon icon icon-next fa fa-diamond"></i><span>Enter Site</span></button>
+			</a>
+		</div>
+	<?php responsive_entry_bottom(); ?>
+	</div><!-- end of #post-<?php the_ID(); ?> -->
+	<?php responsive_entry_after(); ?>
+	<?php
+		endwhile;
+		
+		get_template_part( 'loop-nav', get_post_type() );
+		
+		else :
+		
+		get_template_part( 'loop-no-posts', get_post_type() );
+		
+		endif;
+	?>
 </div><!-- end of #content-full -->
-
 <?php get_footer(); ?>
