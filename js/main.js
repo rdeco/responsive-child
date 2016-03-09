0bf0bb4da9f5fb1199cf0e178c8bfeb33d1de733
rdeco <rdecodesigns@gmail.com>
2016-03-09 13:47:07 -1000

    initial commit

---
+++ js/main.js
@@ -0,0 +1,3 @@
+jQuery( function($) {
+	new cbpScroller( document.getElementById( 'cbp-so-scroller' ) );
+});
---
+++ js/scroller-init.js
@@ -0,0 +1,3 @@
+jQuery(document).ready(function(){
+   new cbpScroller( document.getElementById( 'cbp-so-scroller' ) );
+});
