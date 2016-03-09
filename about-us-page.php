da8dfe7ee9669d1f47e21a178db1de1105708dd9
rdeco <rdecodesigns@gmail.com>
2016-03-09 13:56:01 -1000

    changed layout & effects

--- about-us-page.php
+++ about-us-page.php
@@ -1,61 +1,96 @@
 <?php
     /*
-        Template Name: About Us Page
+        Template Name: About Page
     */
     
-    get_header(); 
-    custom_breadcrumbs(); ?> 
-        <div id="about-me" class="about-me">
-         	<?php
-		 		$args = array(
-		 			'post_type' => 'about-us'	
-            );
-            $query = new WP_Query( $args );
-                while( $query->have_posts() ) :
-                    $query->the_post();
-                    if ($query->current_post % 2 == 0):
-            ?>
-            
-			<div class="about-container">
-                <section class="about-section odd" >
-					<article class="about-article right">	                       
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_field('summary'); ?>	                     
-                    </article>	
-					<aside id="about-content" class="about-content left"> 
-						<p class="about-read-more left">read more</p>
-	                    <figure class="about-figure img-left">		
-	                        <?php  the_post_thumbnail('large'); ?>		
-	                    </figure>
-						<?php the_content(); ?>
-					</aside>
-                </section>
-        	</div>
-        	
-            <?php else: ?>
-            
-            <div class="about-container">
-                <section class="about-section even">
-                
-                	<article class="about-article left">	                      
-                        <h2><?php the_title(); ?></h2>
-                        <?php the_field('summary');?>	                       
-                    </article>
+    get_header(); ?>
+    <?php custom_breadcrumbs(); ?> 
 
-					<div id="about-content" class="about-content right"> 
-						<p class="about-read-more right">read more</p>
-	                    <figure class="about-figure img-right">			
-	                        <?php the_post_thumbnail('large'); ?>
-						</figure>
-						<?php the_content(); ?>
-					</div>
-                </section>
-            </div>     
- 
-            <?php endif; ?>
-            <?php endwhile; ?>
-           
-        </div> <!-- .about-me-->
+	            <div id="cbp-so-scroller" class="cbp-so-scroller">
+	             	<?php
+				 		$args = array(
+				 			'post_type' => 'about-us',
+				 			'posts_per_page' => 6,	
+	                );
+	                $portfolio = new WP_Query( $args );
+	                    while( $portfolio->have_posts() ) :
+	                        $portfolio->the_post();
+	                        if ($portfolio->current_post % 2 == 0):
+	                ?>
+	
+	                <section class="cbp-so-section">
+	                    <article class="cbp-so-side cbp-so-side-left">
+	                        <div class="title"><?php the_field('title'); ?></div>
+	                        <div class="headline"><?php the_field('headline'); ?></div>
+	                        <div class="paragraph"><?php the_field('paragraph'); ?></div>	
+	                        <a href="<?php bloginfo('url'); ?>/contact">
+								<button class="button about button--shikoba">
+									<i class="button__icon icon icon-next fa fa-pencil"></i><span>Contact Me</span>
+								</button>
+							</a>  
+	                        <a href="<?php bloginfo('url'); ?>/blog">
+								<button class="button about button--shikoba">
+									<i class="button__icon icon icon-next fa fa-newspaper-o"></i><span>Blog</span>
+								</button>
+							</a>							                      
+	                    </article>
+	                    <aside>
+	
+	                    </aside>
+	                    <figure class="cbp-so-side cbp-so-side-right">
+	
+	                        <?php
+	                            if ( has_post_thumbnail() ) {
+	                                echo the_post_thumbnail('portfolio-thumb');
+	                        ?>
+	                        <?php }
+	                        else {  ?>
+	                            <img src="<?php print IMAGES; ?>/alt-img_500x400.jpg" alt="img02">
+	                        <?php } ?>
+	
+	                    </figure>
+	                </section>
+	
+	                <?php else: ?>
+	
+	                <section class="cbp-so-section">
+	                    <figure class="cbp-so-side cbp-so-side-left">
+	
+	                        <?php
+	                            if ( has_post_thumbnail() ) {
+	                                echo the_post_thumbnail('portfolio-thumb');
+	                        ?>
+	                        <?php }
+	                        else {  ?>
+	                            <img src="<?php print IMAGES; ?>/alt-img_500x350.jpg" alt="img02">
+	                        <?php } ?>
+	                        
+	                    </figure>
+	                    <article class="cbp-so-side cbp-so-side-right">
+	                        <div class="title"><?php the_field('title'); ?></div>
+	                        <div class="headline"><?php the_field('headline'); ?></div>
+	                        <div class="paragraph"><?php the_field('paragraph'); ?></div>
+	                        	                        
+	                        <a href="<?php bloginfo('url'); ?>/contact">
+								<button class="button about button--shikoba">
+									<i class="button__icon icon icon-next fa fa-pencil"></i><span>Contact Me</span>
+								</button>
+							</a>  
+	                        <a href="<?php bloginfo('url'); ?>/blog">
+								<button class="button about button--shikoba">
+									<i class="button__icon icon icon-next fa fa-newspaper-o"></i><span>Blog</span>
+								</button>
+							</a>	                        
+	                    </article>
+	                </section>
+	
+	                   <?php endif; ?>
+	                <?php endwhile; ?>
+	            </div> <!-- .cbp-so-scroller -->
+
+				</section>
+				
+
 
 <?php get_footer(); ?>
 
